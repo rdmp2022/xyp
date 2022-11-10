8c5e4b35469238dcdd90a1d7a4b66298b6cd6f56
rdmp2022 <wmyaoo@outlook.com>
2022-11-10 14:36:28 +0800

    knife4j

--- xyp/src/main/java/com/sxu/xyp/controller/UserController.java
+++ xyp/src/main/java/com/sxu/xyp/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.sxu.xyp.controller;
 
 import com.sxu.xyp.common.BaseResponse;
-import com.sxu.xyp.common.ErrorCode;
 import com.sxu.xyp.common.ResultUtil;
-import com.sxu.xyp.exception.BusinessException;
 import com.sxu.xyp.model.domain.User;
-import com.sxu.xyp.model.request.LoginRequest;
-import com.sxu.xyp.model.request.RegisterRequest;
+
 import com.sxu.xyp.service.UserService;
-import io.swagger.annotations.Api;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -17,31 +14,31 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = {"http://localhost:3579"})
-@Api(value = "用户API")
+@Api(tags = "用户API")
 public class UserController {
 
     @Resource
     private UserService userService;
 
+    @ApiOperation(value = "用户注册", notes = "账号不小于4位不可包含特殊字符，密码不小于8位，账号密码必须一致", httpMethod = "POST")
     @PostMapping("/register")
-    public BaseResponse<Long> userRegister(@RequestBody RegisterRequest registerRequest){
-        if (registerRequest == null){
-            throw new BusinessException(ErrorCode.PARAMS_ERROR);
-        }
-        String userAccount = registerRequest.getUserAccount();
-        String userPassword = registerRequest.getUserPassword();
-        String checkPassword = registerRequest.getCheckPassword();
+    public BaseResponse<Long> userRegister(
+            @ApiParam(name = "userAccount", value = "账号", required = true) @RequestParam String userAccount,
+            @ApiParam(name = "userPassword", value = "密码", required = true) @RequestParam String userPassword,
+            @ApiParam(name = "checkPassword", value = "确认密码", required = true) @RequestParam String checkPassword
+
+    ){
         long result = userService.userRegister(userAccount, userPassword, checkPassword);
         return ResultUtil.success(result);
     }
 
+    @ApiOperation(value = "用户登录", notes = "账号不小于4位不可包含特殊字符，密码不小于8位", httpMethod = "POST")
     @PostMapping("/login")
-    public BaseResponse<User> userLogin(@RequestBody LoginRequest loginRequest, HttpServletRequest httpServletRequest){
-        if (loginRequest == null){
-            throw new BusinessException(ErrorCode.PARAMS_ERROR);
-        }
-        String userAccount = loginRequest.getUserAccount();
-        String userPassword = loginRequest.getUserPassword();
+    public BaseResponse<User> userLogin(
+            @ApiParam(name = "userAccount", value = "账号", required = true) @RequestParam String userAccount,
+            @ApiParam(name = "userPassword", value = "密码", required = true) @RequestParam String userPassword,
+            HttpServletRequest httpServletRequest
+    ){
         User user = userService.userLogin(userAccount, userPassword, httpServletRequest);
         return ResultUtil.success(user);
     }
--- xyp/src/main/java/com/sxu/xyp/model/domain/User.java
+++ xyp/src/main/java/com/sxu/xyp/model/domain/User.java
@@ -1,6 +1,5 @@
 package com.sxu.xyp.model.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
